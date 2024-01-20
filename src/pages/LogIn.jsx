683cc011658baf75c13cf66800d510126c96bdd3
runtee <ekwo.chinonso.01@gmail.com>
2024-01-20 11:56:50 +0100

    changed login

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,25 +1,25 @@
-import MainLayout from '../components/MainLayout';
 import { Link } from 'react-router-dom';
+import MainLayout from '../components/MainLayout';
 
 const LogIn = () => {
   return (
-    <div className=' bg-black'>
+    <div className=' bg-gray-200'>
       <MainLayout>
-        <div className=" flex flex-col bg-black p-10 rounded-lg my-32 mx-auto w-[40%] max-xl:w-[60%] max-lg:w-[70%] max-sm:w-[100%] max-sm:p-1">
-          <div className="h-[15rem] px-5 flex flex-col justify-center gap-5 text-white border-2 border-solid border-green-300 border-b-0 rounded-lg rounded-bl-0 rounded-br-0">
-            
-              {' '}
-               <p className='text-[2rem] max-sm:text-[1.5rem]'>Welcome To <br /> <span className='text-green-500  '>VaultCoinLimited</span> </p>
-              <p>
-                Our goal is to provide our investors with a reliable source of
-                high income, join us today.
-              </p>
-            
+        <div className=" flex flex-col bg-white p-10 rounded-lg my-32 mx-auto w-[40%] max-xl:w-[60%] max-lg:w-[70%] max-sm:w-[100%] max-sm:p-1">
+          <div className="h-[15rem] px-5 flex flex-col justify-center gap-5 text-black border-2 border-solid border-green-300 border-b-0 rounded-lg rounded-bl-0 rounded-br-0">
+
+            {' '}
+            <p className='text-[2rem] max-sm:text-[1.5rem]'>Welcome To <br /> <span className='text-green-500  '>VaultCoinLimited</span> </p>
+            <p>
+              Our goal is to provide our investors with a reliable source of
+              high income, join us today.
+            </p>
+
           </div>
           <form
             action=""
-            className="text-white flex flex-col p-10 w-[100%] rounded-lg justify-center items-center border-2 border-solid border-green-300 py-10 px-5 "
-            style={{ boxShadow: '0 3px 15px #D4B71680' }}
+            className="text-gray-600 flex flex-col p-10 w-[100%] rounded-lg justify-center items-center border-2 border-solid border-green-300 py-10 px-5 "
+          // style={{ boxShadow: '0 3px 15px ' }}
           >
             <div className="flex flex-col gap-4 mb-5 w-[100%]">
               <label htmlFor="">FirstName</label>
@@ -48,7 +48,7 @@ const LogIn = () => {
             <div className="flex flex-col gap-4 mb-5 w-[100%]">
               <label htmlFor="">Mobile</label>
               <div className="flex ">
-                <button className="relative  w-[5rem] rounded-lg bg-green-500  rounded-r-none text-white">
+                <button className="relative  w-[5rem] rounded-lg bg-green-500  rounded-r-none text-gray-600">
                   +234
                 </button>
                 <input
@@ -65,7 +65,7 @@ const LogIn = () => {
               />
             </div>
             <div className="flex flex-col gap-4 mb-5  w-[100%]">
-              <label htmlFor="">User Name</label>
+              <label htmlFor="">Username</label>
               <input
                 type="text"
                 className="border-2 border-solid border-green-300 focus:border-green-500  bg-[transparent]  outline-none rounded-lg px-2 py-3 w-[100%]"
@@ -91,7 +91,7 @@ const LogIn = () => {
               <input
                 placeholder="Enter code"
                 type="text"
-                className="text-white border-2 border-solid border-green-300 focus:border-green-500  bg-[transparent]  outline-none rounded-lg px-2 py-3 w-[100%]"
+                className="text-gray-600 border-2 border-solid border-green-300 focus:border-green-500  bg-[transparent]  outline-none rounded-lg px-2 py-3 w-[100%]"
               />
             </div>
             <div className="flex items-center gap-3 ">
@@ -109,11 +109,11 @@ const LogIn = () => {
             </div>
             <div className=" w-[100%]">
               <button className="text-white font-bold my-5 py-3 px-4 rounded-lg bg-green-500 ">
-                SignUp Now
+                SignUp
               </button>
               <p>
                 Have an account?{' '}
-               <Link to="/Dashboard"> <span className="text-green-500  cursor-pointer">Login</span>{' '}</Link>
+                <Link to="/Dashboard"> <span className="text-green-500  cursor-pointer">Login</span>{' '}</Link>
               </p>
             </div>
           </form>
